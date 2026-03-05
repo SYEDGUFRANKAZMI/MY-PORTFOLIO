be9e5a5a7d774cfea36647b3104695d41552c6ec
SYED GUFRAN KAZMI <97060779+SYEDGUFRANKAZMI@users.noreply.github.com>
2026-03-05 17:33:18 +0530

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MY_PORTFOLIO.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
